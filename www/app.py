5160a40a55e767f97c7f5e879c15f0640e392533
zpjayyy <zp1988914@163.com>
2016-11-10 16:22:45 +0800

    feat: signin func

--- www/app.py
+++ www/app.py
@@ -11,6 +11,8 @@ from jinja2 import Environment, FileSystemLoader
 import www.orm
 from www.coreweb import add_routes, add_static
 
+from www.handlers import cookie2user, COOKIE_NAME
+
 
 def init_jinja2(app, **kw):
     logging.info('init jinjia2...')
@@ -59,6 +61,23 @@ async def data_factory(app, handler):
         return (await handler(request))
 
     return parse_data
+
+async def auth_factory(app, handler):
+    async def auth(request):
+        logging.info('check user: %s %s' % (request.method, request.path))
+
+        request.__user__ = None
+        cookie_str = request.cookies.get(COOKIE_NAME)
+        if cookie_str:
+            user = await cookie2user(cookie_str)
+            if user:
+                logging.info('set current user: %s' % user.email)
+                request.__user__ = user
+        if request.path.startswith('/manage/') and (request.__user__ is None or not request.__user__.admin):
+            return web.HTTPNotFound('/signin')
+        return (await handler(request))
+
+    return auth
 
 
 async def response_factory(app, handler):
@@ -131,7 +150,7 @@ async def init(loop):
     await www.orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user='www-data', password='www-data',
                               db='awesome')
     app = web.Application(loop=loop, middlewares=[
-        logger_factory, response_factory
+        logger_factory, auth_factory, response_factory
     ])
     init_jinja2(app, filters=dict(datetime=datetime_filter))
     add_routes(app, 'handlers')
--- www/handlers.py
+++ www/handlers.py
@@ -31,6 +31,39 @@ def user2cookie(user, max_age):
     return '-'.join(L)
 
 
+async def cookie2user(cookie_str):
+    '''
+    Parse cookie and load user if cookie is valid.
+    :param cookie_str:
+    :return:
+    '''
+
+    if not cookie_str:
+        return None
+
+    try:
+        L = cookie_str.split('-')
+        if len(L) != 3:
+            return None
+        uid, expires, sha1 = L
+        if int(expires) < time.time():
+            return None
+
+        user = await User.find(uid)
+        if user is None:
+            return None
+
+        s = '%s-%s-%s-%s' % (uid, user.passwd, expires, _COOKIE_KEY)
+        if sha1 != hashlib.sha1(s.encode('utf-8')).hexdigest():
+            logging.info('invalid sha1')
+            return None
+        user.passwd = '******'
+        return user
+    except Exception as e:
+        logging.exception(e)
+        return None
+
+
 @get('/')
 async def index(request):
     summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
@@ -52,8 +85,43 @@ def register():
     }
 
 
+@get('/signin')
+def signin():
+    return {
+        '__template__': 'signin.html'
+    }
+
+
 _RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
 _RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
+
+
+@post('/api/authenticate')
+async def authenticate(*, email, passwd):
+    if not email:
+        raise APIValueError('email', 'Invalid email.')
+    if not passwd:
+        raise APIValueError('passwd', 'Invalid email.')
+
+    users = await User.findAll('email=?', [email])
+    if len(users) == 0:
+        raise APIValueError('email', 'Email not exist.')
+
+    user = users[0]
+
+    sha1 = hashlib.sha1()
+    sha1.update(user.id.encode('utf-8'))
+    sha1.update(b':')
+    sha1.update(passwd.encode('utf-8'))
+    if user.passwd != sha1.hexdigest():
+        raise APIValueError('passwd', 'Invalid password.')
+
+    r = web.Response()
+    r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
+    user.passwd = '******'
+    r.content_type = 'application/json'
+    r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
+    return r
 
 
 @post('/api/users')
