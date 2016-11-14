364572d977f46e3a7e73b0e0f2b941a498b155b3
zpjayyy <zp1988914@163.com>
2016-11-14 11:26:32 +0800

    feat: manage blog func

--- www/handlers.py
+++ www/handlers.py
@@ -2,19 +2,35 @@
 
 import re, time, json, logging, hashlib, base64, asyncio
 
+import www.markdown2
+
 from aiohttp import web
 
 from www.coreweb import get, post
-from www.apis import APIError, APIValueError, APIResourceNotFoundError
+from www.apis import APIError, APIValueError, APIResourceNotFoundError, APIPermissionError, Page
 
 from www.models import User, Comment, Blog, next_id
 from www.config import configs
 
 logging.basicConfig(level=logging.INFO)
 
-
 COOKIE_NAME = 'awesession'
 _COOKIE_KEY = configs.session.secret
+
+
+def check_admin(request):
+    if request.__user__ is None or not request.__user__.admin:
+        raise APIPermissionError()
+
+def get_page_index(page_str):
+    p = 1
+    try:
+        p = int(page_str)
+    except ValueError as e:
+        pass
+    if p < 1:
+        p = 1
+    return p
 
 
 def user2cookie(user, max_age):
@@ -64,6 +80,12 @@ async def cookie2user(cookie_str):
         return None
 
 
+def text2html(text):
+    lines = map(lambda s: '<p>%s</p>' % s.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;'),
+                filter(lambda s: s.strip() != '', text.split('\n')))
+    return ''.join(lines)
+
+
 @get('/')
 async def index(request):
     summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
@@ -74,7 +96,8 @@ async def index(request):
     ]
     return {
         '__template__': 'blogs.html',
-        'blogs': blogs
+        'blogs': blogs,
+        '__user__': request.__user__
     }
 
 
@@ -89,6 +112,37 @@ def register():
 def signin():
     return {
         '__template__': 'signin.html'
+    }
+
+
+@get('/blog/{id}')
+async def get_blog(id):
+    blog = await Blog.find(id)
+    comments = await Comment.findAll('blog_id=?', [id], orderBy='create_at desc')
+    for c in comments:
+        c.html_content = text2html(c.content)
+    blog.html_content = www.markdown2.markdown(blog.content)
+    return {
+        '__template__': 'blog_html',
+        'blog': blog,
+        'comments': comments
+    }
+
+
+@get('/manage/blogs')
+def manage_blogs(*, page='1'):
+    return {
+        '__template__': 'manage_blogs.html',
+        'page_index': get_page_index(page)
+    }
+
+
+@get('/manage/blogs/create')
+def manage_create_blog():
+    return {
+        '__template__': 'manage_blog_edit.html',
+        'id': '',
+        'action': '/api/blogs'
     }
 
 
@@ -149,3 +203,40 @@ async def api_register_user(*, email, name, passwd):
     r.content_type = 'application/json'
     r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
     return r
+
+
+@get('/api/blogs')
+async def ap_blogs(*, page='1'):
+    page_index = get_page_index(page)
+    num = await Blog.findNumber('count(id)')
+    p = Page(num, page_index)
+
+    if num == 0:
+        return dict(page=p, blog=())
+
+    blogs = await Blog.findAll(orderBy='created_at desc', limit=(p.offset, p.limit))
+
+    return dict(page=p, blogs=blogs)
+
+
+
+@get('/api/blogs/{id}')
+async def api_get_blog(*, id):
+    blog = await Blog.find(id)
+    return blog
+
+
+@post('/api/blogs')
+async def api_create_blog(request, *, name, summary, content):
+    check_admin(request)
+    if not name or not name.strip():
+        raise APIValueError('name', 'name cannot be empty.')
+    if not summary or not summary.strip():
+        raise APIValueError('summary', 'summary cannot be empty.')
+    if not content or not content.strip():
+        raise APIValueError('content', 'content cannot be empty.')
+
+    blog = Blog(user_id=request.__user__.id, user_name=request.__user__.name, user_image=request.__user__.image,
+                name=name.strip(), summary=summary.strip(), content=content.strip())
+    await blog.save()
+    return blog
--- www/models.py
+++ www/models.py
@@ -20,7 +20,7 @@ class User(Model):
 
 
 class Blog(Model):
-    __table__ = 'blog'
+    __table__ = 'blogs'
 
     id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
     user_id = StringField(ddl='varchar(50)')
